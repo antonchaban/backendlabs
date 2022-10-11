56ed98f9be915b45749160899b686f8d14321561
Anton Chaban <71810009+antonchaban@users.noreply.github.com>
2022-10-11 17:53:23 +0300

    Backend Lab1 init

---
+++ src/main/java/ua/kpi/fict/backendlabs/BackendlabsApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.fict.backendlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendlabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendlabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kpi/fict/backendlabs/BackendlabsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.fict.backendlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendlabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
